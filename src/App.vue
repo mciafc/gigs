8ad36e3068cba2d2c2547549a2034d9f03afed13
carreb <caleb@domyjobfor.me>
2022-10-25 11:19:32 -0400

    Start implementing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GigComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GigComponent from "./components/GigComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GigComponent
   }
 }
 </script>
