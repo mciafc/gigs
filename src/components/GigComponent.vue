2ab65dd0638c1383faf3f3b9f3ecfef32d45b6fa
carreb <caleb@domyjobfor.me>
2022-10-26 14:53:00 -0400

    add past gigs section & headers

--- src/components/GigComponent.vue
+++ src/components/GigComponent.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
+    <h1>Upcoming Gigs:</h1>
+    <p>Sorted in order of soonest to latest</p>
         <div v-if="gigs">
-            <div :for="gig in gigs" :key="gig._id">
+            <div class="gig" :for="gig in gigs" :key="gig._id">
                 <h1>gig.gigName</h1>
                 <h3>By: gig.organizationName</h3>
             </div>
         </div>
         <div v-else>
             <h1>There are no upcoming gigs currently scheduled.</h1>
+        </div>
+    <h1>Previous Gigs:</h1>
+        <div ref="pastGigsDiv">
+            <div class="gig" :for="gig in pastgigs" :key="gig._id">
+                <h1>gig.gigName</h1>
+                <h3>By: gig.organizationName</h3>
+            </div>
         </div>
   </div>
 </template>
